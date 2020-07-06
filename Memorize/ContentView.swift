25691524f5058ce1db26b39476b37d139c8dc38d
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2020-07-06 20:37:51 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Vinnicius Gomes on 06/07/20.
+//  Copyright © 2020 Vinnicius Gomes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
